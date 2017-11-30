41d87cd561329a06eb6f7a01fd13c86f7acc531e
= <ousmanefouchimi@gmail.com>
2017-11-30 14:45:21 -0700

    Replace obsolete md-toolbar by mat-toolbar selector tag in app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatToolbarModule } from '@angular/material';
+import { MatToolbarModule} from '@angular/material/toolbar';
 import { HttpModule } from '@angular/http';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
